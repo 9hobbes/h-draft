09631563ffeb3b504b57b0c17c36a0c8a9006386
Hyunrae Cho <9beach@gmail.com>
2018-08-19 01:27:17 +0900

    Add draft-js components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HEditor from './h-editor';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to H-Draft</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          두 줄 밑에 중간에 잘 눌러보면 커서가 생기고 편집할 수 있습니다.
         </p>
+        <HEditor />
       </div>
     );
   }
